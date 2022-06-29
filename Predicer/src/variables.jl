0a77b6523543ce31a058f5406d0f0d220fe2772c
Helmi Hankimaa <helmi.hankimaa@gmail.com>
2022-06-29 14:35:14 +0300

    Changed variable creating functions to call tuple functions not model_contents.

--- Predicer/src/variables.jl
+++ Predicer/src/variables.jl
@@ -9,42 +9,42 @@ Create the variables used in the model, and save them in the model_contents dict
 # Arguments
 - `model_contents::OrderedDict`: Dictionary containing all data and structures used in the model. 
 """
-function create_variables(model_contents::OrderedDict)
-    create_v_flow(model_contents)
-    create_v_online(model_contents)
-    create_v_reserve(model_contents)
-    create_v_state(model_contents)
-    create_v_flow_op(model_contents)
-    create_v_risk(model_contents)
+function create_variables(model_contents::OrderedDict, input_data::InputData)
+    create_v_flow(model_contents, input_data)
+    create_v_online(model_contents, input_data)
+    create_v_reserve(model_contents, input_data)
+    create_v_state(model_contents, input_data)
+    create_v_flow_op(model_contents, input_data)
+    create_v_risk(model_contents, input_data)
 end
 
 
 """
-    create_v_flow(model_contents::OrderedDict)
+    create_v_flow(model_contents::OrderedDict, input_data::InputData)
 
-Sets up v_flow, which is the variable symbolising flows between nodes/processes.
+Set up v_flow variables, which symbolise flows between nodes and processes.
 
 # Arguments
 - `model_contents::OrderedDict`: Dictionary containing all data and structures used in the model. 
 """
-function create_v_flow(model_contents::OrderedDict)
-    process_tuple = model_contents["tuple"]["process_tuple"]
+function create_v_flow(model_contents::OrderedDict, input_data::InputData)
+    process_tuples = process_topology_tuples(input_data)
     model = model_contents["model"]
-    v_flow = @variable(model, v_flow[tup in process_tuple] >= 0)
+    v_flow = @variable(model, v_flow[tup in process_tuples] >= 0)
     model_contents["variable"]["v_flow"] = v_flow
 end
 
 
 """
-    create_v_online(model_contents::OrderedDict)
+    create_v_online(model_contents::OrderedDict, input_data::InputData)
 
-Sets up variables used for modelling functionality for processes with binary online variables, as well as starts/stops.
+Set up binary online, start and stop variables for modeling online functionality for processes.
 
 # Arguments
 - `model_contents::OrderedDict`: Dictionary containing all data and structures used in the model. 
 """
-function create_v_online(model_contents::OrderedDict)
-    proc_online_tuple = model_contents["tuple"]["proc_online_tuple"]
+function create_v_online(model_contents::OrderedDict, input_data::InputData)
+    proc_online_tuple = online_process_tuples(input_data)
     if !isempty(proc_online_tuple)
         model = model_contents["model"]
         v_online = @variable(model, v_online[tup in proc_online_tuple], Bin)
@@ -58,29 +58,29 @@ end
 
 
 """
-    create_v_reserve(model_contents::OrderedDict)
+    create_v_reserve(model_contents::OrderedDict, input_data::InputData)
 
-Sets up the variables used for modelling reserves.
+Set up process, node and bid reserve variables used for modelling reserves.
 
 # Arguments
 - `model_contents::OrderedDict`: Dictionary containing all data and structures used in the model. 
 """
-function create_v_reserve(model_contents::OrderedDict)
+function create_v_reserve(model_contents::OrderedDict, input_data::InputData)
     model = model_contents["model"]
 
-    res_potential_tuple = model_contents["tuple"]["res_potential_tuple"]
+    res_potential_tuple = reserve_process_tuples(input_data)
     if !isempty(res_potential_tuple)
        v_reserve = @variable(model, v_reserve[tup in res_potential_tuple] >= 0)
        model_contents["variable"]["v_reserve"] = v_reserve
     end
 
-    res_tuple = model_contents["tuple"]["res_tuple"]
+    res_tuple = reserve_market_directional_tuples(input_data)
     if !isempty(res_tuple)
         v_res = @variable(model, v_res[tup in res_tuple] >= 0)
         model_contents["variable"]["v_res"] = v_res
     end
 
-    res_final_tuple = model_contents["tuple"]["res_final_tuple"]
+    res_final_tuple = reserve_market_tuples(input_data)
     if !isempty(res_final_tuple)
         @variable(model, v_res_final[tup in res_final_tuple] >= 0)
         model_contents["variable"]["v_res_final"] = v_res_final
@@ -89,24 +89,24 @@ end
 
 
 """
-    create_v_state(model_contents::OrderedDict)
+    create_v_state(model_contents::OrderedDict, input_data::InputData)
 
-Sets up variables used for node state (storage) functionality.
+Set up state variables and surplus and shortage slack variables used for modeling node state (storage).
 
 # Arguments
 - `model_contents::OrderedDict`: Dictionary containing all data and structures used in the model. 
 """
-function create_v_state(model_contents::OrderedDict)
+function create_v_state(model_contents::OrderedDict, input_data::InputData)
     model = model_contents["model"]
-    node_state_tuple = model_contents["tuple"]["node_state_tuple"]
-    node_balance_tuple = model_contents["tuple"]["node_balance_tuple"]
+    node_state_tuple = state_node_tuples(input_data)
+    node_balance_tuple = balance_node_tuples(input_data)
 
 
     # Node state variable
     v_state = @variable(model, v_state[tup in node_state_tuple] >= 0)
     model_contents["variable"]["v_state"] = v_state
 
-    # Dummy variables for node_states
+    # Slack variables for node_states
     vq_state_up = @variable(model, vq_state_up[tup in node_balance_tuple] >= 0)
     vq_state_dw = @variable(model, vq_state_dw[tup in node_balance_tuple] >= 0)
     model_contents["variable"]["vq_state_up"] = vq_state_up
@@ -115,16 +115,16 @@ end
 
 
 """
-    create_v_flow_op(model_contents::OrderedDict)
+    create_v_flow_op(model_contents::OrderedDict, input_data::InputData)
 
-Sets up variables for processes with piecewise efficiency functionality.
+Set up operational slot flow variables and binary slot indicator variable for processes with piecewise efficiency functionality.
 
 # Arguments
 - `model_contents::OrderedDict`: Dictionary containing all data and structures used in the model. 
 """
-function create_v_flow_op(model_contents::OrderedDict)
+function create_v_flow_op(model_contents::OrderedDict, input_data::InputData)
     model = model_contents["model"]
-    proc_op_balance_tuple = model_contents["tuple"]["proc_op_balance_tuple"]
+    proc_op_balance_tuple = operative_slot_process_tuples(input_data)
     v_flow_op_in = @variable(model,v_flow_op_in[tup in proc_op_balance_tuple] >= 0)
     v_flow_op_out = @variable(model,v_flow_op_out[tup in proc_op_balance_tuple] >= 0)
     v_flow_op_bin = @variable(model,v_flow_op_bin[tup in proc_op_balance_tuple], Bin)
@@ -135,16 +135,16 @@ end
 
 
 """
-    create_v_risk(model_contents::OrderedDict)
+    create_v_risk(model_contents::OrderedDict, input_data::InputData)
 
-Sets up variables for risk element.
+Set up variables for CVaR risk measure.
 
 # Arguments
 - `model_contents::OrderedDict`: Dictionary containing all data and structures used in the model. 
 """
-function create_v_risk(model_contents::OrderedDict)
+function create_v_risk(model_contents::OrderedDict, input_data::InputData)
     model = model_contents["model"]
-    risk_tuple = model_contents["tuple"]["risk_tuple"]
+    risk_tuple = scenarios(input_data)
     v_var = @variable(model,v_var)
     v_cvar_z = @variable(model,v_cvar_z[tup in risk_tuple] >= 0)
     model_contents["variable"]["v_var"] = v_var
