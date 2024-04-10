f1e50b9932d298edf40371a49f329e5f8f2609f1
Timo Korvola <Timo.Korvola@vtt.fi>
2024-04-10 15:01:41 +0300

    Add some tests.

---
+++ test/runtests.jl
@@ -0,0 +1,16 @@
+using Predicer
+using Test
+using JuMP
+
+# Model definition files and objective values
+cases = Dict(
+    "input_data.xlsx" => -12031.87393643243,
+    "demo_model.xlsx" => -1095.5118308122817)
+
+@testset "Predicer on $bn" for (bn, obj) in cases
+    (mc, inp) = Predicer.generate_model(joinpath("../input_data", bn))
+    Predicer.solve_model(mc)
+    m = mc["model"]
+    @test termination_status(m) == MOI.OPTIMAL
+    @test objective_value(m) ≈ obj atol=1e-8
+end
