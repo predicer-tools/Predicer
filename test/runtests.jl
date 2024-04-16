9cba7ebe8325fc8e28fd8e35c5774ac31f20ab37
Timo Korvola <Timo.Korvola@vtt.fi>
2024-04-16 19:02:16 +0300

    More test cases.
    Include all models in input_data.

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,15 @@ using HiGHS
 optr = HiGHS.Optimizer
 
 # Model definition files and objective values
-cases = Dict(
-    "input_data.xlsx" => -12031.87393643243,
-    "demo_model.xlsx" => -1095.5118308122817)
+cases = [
+    "input_data.xlsx" => -12031.87393643243
+    "demo_model.xlsx" => -1095.5118308122817
+    "simple_building_model.xlsx" => 396.59132448327125
+    "example_model.xlsx" => -11014.127894223102
+    "input_data_common_start.xlsx" => -1593.5748049230276
+    "input_data_delays.xlsx" => 62.22222222222222
+    "input_data_temps.xlsx" => 6.828673104147784e6
+]
 
 @testset "Predicer on $bn" for (bn, obj) in cases
     inp = Predicer.get_data(joinpath("../input_data", bn))
