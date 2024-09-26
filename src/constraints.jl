5522c9af0f74307b407189920e3fd1e860af047b
Timo Korvola <Timo.Korvola@vtt.fi>
2024-09-26 18:14:42 +0300

    Cosmetic editing

--- src/constraints.jl
+++ src/constraints.jl
@@ -1257,24 +1257,24 @@ function setup_bidding_curve_constraints(model_contents::OrderedDict, input_data
         m_prod_flow = proc_tup_in[m]
         m_cons_bal_flow = (m, "dw")
         m_prod_bal_flow = (m, "up")
-        for s in scenarios(input_data), (t, ti) in input_data.temporals.times
-            tup = (m, s, t)
+        for s in scenarios(input_data), (ts, t) in input_data.temporals.times
+            tup = (m, s, ts)
             v_bid[tup] = AffExpr(0.0)
             e_bid_slot[tup] = AffExpr(0.0)
             if markets[m].m_type == "energy"
-                add_to_expression!(v_bid[tup],v_flow[validate_tuple(val_dict, common_ts, (m_prod_flow..., s, t), 4)],1.0) #prod 
-                add_to_expression!(v_bid[tup],v_flow_bal[validate_tuple(val_dict, common_ts, (m_prod_bal_flow..., s, t), 3)],1.0) #prod_bal
-                add_to_expression!(v_bid[tup],v_flow[validate_tuple(val_dict, common_ts, (m_cons_flow..., s, t), 4)],-1.0) #cons
-                add_to_expression!(v_bid[tup],v_flow_bal[validate_tuple(val_dict, common_ts, (m_cons_bal_flow..., s, t), 3)],-1.0) #cons_bal
+                add_to_expression!(v_bid[tup],v_flow[validate_tuple(val_dict, common_ts, (m_prod_flow..., s, ts), 4)],1.0) #prod 
+                add_to_expression!(v_bid[tup],v_flow_bal[validate_tuple(val_dict, common_ts, (m_prod_bal_flow..., s, ts), 3)],1.0) #prod_bal
+                add_to_expression!(v_bid[tup],v_flow[validate_tuple(val_dict, common_ts, (m_cons_flow..., s, ts), 4)],-1.0) #cons
+                add_to_expression!(v_bid[tup],v_flow_bal[validate_tuple(val_dict, common_ts, (m_cons_bal_flow..., s, ts), 3)],-1.0) #cons_bal
             else
                 add_to_expression!(v_bid[tup],v_res_final[tup],1.0)
             end
-            (bn0, bn1) = b_slots[m].market_price_allocation[(s, ti)]
-            p0 = b_slots[m].prices[(ti, bn0)]
-            p1 = b_slots[m].prices[(ti, bn1)]
-            ps = markets[m].price(s, ti)
-            add_to_expression!(e_bid_slot[tup],v_bid_vol[(m,bn0,t)],1-(ps-p0)/(p1-p0))
-            add_to_expression!(e_bid_slot[tup],v_bid_vol[(m,bn1,t)],(ps-p0)/(p1-p0))
+            (bn0, bn1) = b_slots[m].market_price_allocation[(s, t)]
+            p0 = b_slots[m].prices[(t, bn0)]
+            p1 = b_slots[m].prices[(t, bn1)]
+            ps = markets[m].price(s, t)
+            add_to_expression!(e_bid_slot[tup],v_bid_vol[(m,bn0,ts)],1-(ps-p0)/(p1-p0))
+            add_to_expression!(e_bid_slot[tup],v_bid_vol[(m,bn1,ts)],(ps-p0)/(p1-p0))
         end
     end
     bid_scen_tuple = Predicer.bid_scenario_tuples(input_data)
@@ -1629,7 +1629,6 @@ function setup_generic_constraints(model_contents::OrderedDict, input_data::Pred
                         t = const_ts[c]],
                 const_expr[c][(s, t)] in const_set[c])
     model_contents["gen_constraint"] = gen_con
-    tup_it(tup) = (tup[1], tup[2], temporals.times[tup[3]])
     @constraint(
         model, setpoint_eq[c = setpoints, s = scenarios(input_data),
                            t = values(temporals.times)],
