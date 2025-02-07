eb7c326a0f7ee3bd3cdb0fb60d9cd241768ec86e
sundelld <dennis.sundell@vtt.fi>
2025-02-07 12:40:33 +0200

    Update runtests.jl
    Added a short test for the rolling model.

--- test/runtests.jl
+++ test/runtests.jl
@@ -69,3 +69,10 @@ end
     @test !isempty([Predicer.get_node_balance(mc, inp, n, s) for n in collect(keys(inp.nodes))])
     
 end
+
+roll_data_log = Predicer.roll_predicer(joinpath("..", "input_data", "rolling_model.xlsx"), 24, 6, 0, 48)
+@test haskey(roll_data_log, "model_runs")
+@test haskey(roll_data_log, "runs_info")
+@test haskey(roll_data_log, "meta")
+@test !haskey(roll_data_log, "failed_last_run")
+@test ≈(sum(sum(eachcol(Predicer.collect_cost_dfs(roll_data_log)[!, 3:11]))), 18311.28, atol=0.01)
